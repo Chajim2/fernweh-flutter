534f5476c246125fd5233341d2d15259b1137d56
Chajim2 <jachym.dytrt@gmail.com>
2026-06-25 11:32:40 +0200

    swapped to sqlite

--- fernweh_backend/app/database.py
+++ fernweh_backend/app/database.py
@@ -5,17 +5,17 @@ from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-from sqlmodel.ext.asyncio.session import AsyncSession # <-- CORRECT AsyncSession
+from sqlmodel.ext.asyncio.session import AsyncSession  # <-- CORRECT AsyncSession
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
-DATABASE_URL = os.getenv("DB_URL")
-if DATABASE_URL:
-    ASYNC_DATABASE_URL = DATABASE_URL.replace("postgresql", "postgresql+asyncpg", 1) 
 
-engine = create_engine(DATABASE_URL)
-async_engine = create_async_engine(ASYNC_DATABASE_URL, echo=True, connect_args={"ssl" : "require"})
+DATABASE_URL = "sqlite:///./fernweh.db"
+ASYNC_DATABASE_URL = "sqlite+aiosqlite:///./fernweh.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+async_engine = create_async_engine(ASYNC_DATABASE_URL, echo=True)
 
 
 async def create_db_and_tables():
@@ -29,10 +29,10 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+
 async def get_async_session():
     async with AsyncSession(async_engine) as session:
         yield session
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
-
